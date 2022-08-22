5ec0d407938ef7bd724a1fa546a043e6ca8b54ab
Irineu Junior Pinheiro dos Santos <irineus@gmail.com>
2022-08-22 15:51:21 -0300

    Seed de dados usando os arquivos de migration

---
+++ APICatalog/Migrations/20220822184250_SeedCategories.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalog.Migrations
+{
+    public partial class SeedCategories : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into categories(Name, ImageURL) Values('Bebidas','bebidas.jpg')");
+            mb.Sql("Insert into categories(Name, ImageURL) Values('Lanches','lanches.jpg')");
+            mb.Sql("Insert into categories(Name, ImageURL) Values('Sobremesas','sobremesas.jpg')");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from categories");
+        }
+    }
+}
