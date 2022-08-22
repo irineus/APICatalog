95e6e966a86493432e252fb17f0adcce91e02f98
Irineu Junior Pinheiro dos Santos <irineus@gmail.com>
2022-08-22 15:39:34 -0300

    Migration para a estrutura das tabelas de banco

--- APICatalog/Models/Category.cs
+++ APICatalog/Models/Category.cs
@@ -1,12 +1,25 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace APICatalogo.Models;
 
+[Table("Categories")]
 public class Category
 {
+    [Key]
     public int Id { get; set; }
+
+    [Display(Name = "Nome")]
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [StringLength(maximumLength: 80, MinimumLength = 1, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caracteres")]
     public string? Name { get; set; }
+
+    [Display(Name = "URL da Imagem")]
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [StringLength(maximumLength: 300, MinimumLength = 8, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caracteres")]
     public string? ImageURL { get; set; }
+    
     public ICollection<Product> Products { get; set; }
 
     public Category()
--- APICatalog/Models/Product.cs
+++ APICatalog/Models/Product.cs
@@ -1,21 +1,42 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace APICatalogo.Models;
 
+[Table("Products")]
 public class Product
 {
+    [Key]
     public int Id { get; set; }
-    
+
+    [Display(Name = "Nome")]
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [StringLength(maximumLength: 80, MinimumLength = 1, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caracteres")]
     public string? Name { get; set; }
-    
+
+    [Display(Name = "Descrição")]
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [StringLength(maximumLength: 300, MinimumLength = 8, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caracteres")]
     public string? Description { get; set; }
-    
+
+    [Display(Name = "Preço")]
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [Column(TypeName = "decimal(10,2)")]
+    [Range(100.0, 50000.0, ErrorMessage = "O campo {0} deve ter um valor entre {1} e {2}")]
+    [DataType(DataType.Currency)]
     public decimal Price { get; set; }
-    
+
+    [Display(Name = "URL da Imagem")]
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [StringLength(maximumLength: 300, MinimumLength = 8, ErrorMessage = "O campo {0} deve ter entre {2} e {1} caracteres")]
     public string? ImageURL { get; set; }
-    
+
+    [Display(Name = "Estoque")]
     public float Inventory { get; set; }
-    
+
+    [Display(Name = "Data de Criação")]
+    [DataType(DataType.Date)]
+    [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
     public DateTime CreateDate { get; set; }
     
     public int CategoryId { get; set; }
